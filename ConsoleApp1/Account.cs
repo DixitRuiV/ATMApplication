66ebdf968a2b881a381a4f25a02ac379a849dd39
DixitRuiV <84727744+DixitRuiV@users.noreply.github.com>
2025-11-08 15:20:36 +0000

    Add files via upload

---
+++ ConsoleApp1/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM.Account
+{
+    public class Account
+    {
+        public string Name;
+        public decimal Balance;
+        public int AccountNumber;    
+        public int BranchNumber;
+    }
+}
